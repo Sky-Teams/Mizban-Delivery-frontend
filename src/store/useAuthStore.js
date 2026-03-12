f64e1bf35e8b97592c063b543ccb7c88bcf67f91
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-03-12 05:23:17 +0430

    add confirmPassword with its validation

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -7,6 +7,7 @@ const  useAuthStore=create((set,get) => ({
         name:"",
         email:"",
         password:"",
+        confirmPassword:"",
         phone:"",
     },
 
@@ -32,6 +33,7 @@ const  useAuthStore=create((set,get) => ({
                     name:"",
                     email:"",
                     password:"",
+                    confirmPassword:"",
                     phone:""
                 },
                 errors:{}
@@ -53,8 +55,14 @@ const  useAuthStore=create((set,get) => ({
             newErrors.email = "Email is invalid";
 
             if (!form.password) newErrors.password = "Password is required";
-            else if (form.password.length < 6)
-            newErrors.password = "Password must be at least 6 characters";
+           
+            if(!form.confirmPassword) 
+                newErrors.confirmPassword ="Confirm password is required";
+
+            if(form.password && form.confirmPassword && 
+                form.password !== form.confirmPassword){
+                    newErrors.confirmPassword="Passwords do not match";
+                }
 
             if (!form.phone) newErrors.phone = "Phone is required";
 
@@ -76,7 +84,8 @@ const  useAuthStore=create((set,get) => ({
             setLoading(true);
 
             try {
-            const data = await signup(form);
+            const {name,email,password,phone}=form;    
+            const data = await signup({name, email,password,phone});
 
             toast.dismiss();
             toast.success(data.message);
