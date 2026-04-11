b3020ed819e7503ff87ce1c22adce492a0400549
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-04-11 14:16:15 +0430

    remove try-catch block of authService file

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,22 +1,15 @@
 import api from './api';
-import {handleApiError} from './handleApiError';
 
 export const signup = async(userData) => {
-    try{
-        const response = await api.post('auth/register', {json: userData}).json();
+        const response = await api.post('auth/register', {json: userData})
+        .json();
         return response;
-    }catch(error){
-       await handleApiError(error);
-    }
+    
 };
 
 
 export const login = async (credentials) => {
-   
-    try{
-       const response = await api.post("auth/login", { json: credentials }).json();
+       const response = await api.post("auth/login", { json: credentials })
+       .json();
         return response;
-    }catch(error){
-       await handleApiError(error);
-    }
 };
