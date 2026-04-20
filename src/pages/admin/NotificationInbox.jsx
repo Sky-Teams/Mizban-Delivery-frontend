aa5f0c57d8038d3d12119dbef55df88f2d7f30ef
fataneh-mow <fataneh.mow@gmail.com>
2026-04-20 16:30:42 +0430

    fix: removed event listeners #8

--- src/pages/admin/NotificationInbox.jsx
+++ src/pages/admin/NotificationInbox.jsx
@@ -1,34 +1,13 @@
 import { useEffect, useState } from "react";
-import { socket } from "../../notificationSystem/socket"; 
+import { socket } from "../../config/socket"; 
 import NotificationBox from "../../components/notificationSystem/NotificationBox";
 import { useTranslation } from "react-i18next";
 
 export default function NotificationInbox() {
-    const [notifications, setNotifications] = useState([]);
     const {t} = useTranslation()
-
-    useEffect(() => {
-        const handleNewOrder = (data) => {
-            console.log("New order received:", data);
-            setNotifications(prev => [
-                {
-                    id: data.orderId || Date.now(),
-                    message: data.message || t("NEW_NOTIFICATION")
-                },
-                ...prev
-            ]);
-        };
-
-        socket.on("notification", handleNewOrder);
-
-        return () => {
-            socket.off("notification", handleNewOrder);
-        };
-    }, []);
-
     return (
         <div>
-        <NotificationBox notifications={notifications} />
+            <NotificationBox />
         </div>
     );
 }
