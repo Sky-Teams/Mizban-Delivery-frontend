713315eb7c7601b2cb459e24b7d876305b18177c
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-04-28 22:42:21 +0430

    add utils functions of i18nHelper

--- src/utils/i18nHelper.js
+++ src/utils/i18nHelper.js
@@ -1,5 +1,15 @@
 import i18n from '../i18n';
 
+
+export const isRTL = ()=>{
+    const rtlLanguages = ['fa','ps'];
+    return rtlLanguages.includes(i18n.language);
+}
+
+export const getCurrentLang = () =>{
+    return i18n.language;
+}
+
 export const getServerMessage = (errorData) =>{
     if(errorData.messages){
         const lang = i18n.language;
