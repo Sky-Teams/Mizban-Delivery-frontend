73bd99a3dcc440150689b26ed9e1df4fa4d2ffba
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-04-30 13:32:43 +0430

    remove toast and navigation params

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -4,226 +4,219 @@ import i18n from '../i18n';
 import { getServerMessage } from '../utils/i18nHelper';
 
 const useAuthStore = create((set, get) => ({
-    // form fields
-    form: {
+  // form fields
+  form: {
     name: '',
     email: '',
     password: '',
     confirmPassword: '',
     phone: '',
-    },
+  },
 
-    // error messages
-    errors: {},
+  // error messages
+  errors: {},
 
-    // loading state
-    loading: false,
+  // loading state
+  loading: false,
 
-    user: JSON.parse(localStorage.getItem('user')) || null,
-    token: localStorage.getItem('token') || null,
+  user: JSON.parse(localStorage.getItem('user')) || null,
+  token: localStorage.getItem('token') || null,
 
-    // set single field
-    setField: (field, value) =>
+  // set single field
+  setField: (field, value) =>
     set((state) => ({
-        form: { ...state.form, [field]: value },
+      form: { ...state.form, [field]: value },
     })),
 
-    // set multiple errors
-    setErrors: (errors) => set({ errors }),
+  // set multiple errors
+  setErrors: (errors) => set({ errors }),
 
-    // reset form
-    resetForm: () =>
+  // reset form
+  resetForm: () =>
     set({
-        form: {
+      form: {
         name: '',
         email: '',
         password: '',
         confirmPassword: '',
         phone: '',
-        },
-        errors: {},
+      },
+      errors: {},
     }),
 
-    // set Loading
-    setLoading: (loading) => set({ loading }),
+  // set Loading
+  setLoading: (loading) => set({ loading }),
 
-    //
-    setUser: (user, token) => {
+  //
+  setUser: (user, token) => {
     set({ user, token });
     localStorage.setItem('user', JSON.stringify(user));
     localStorage.setItem('token', token);
-    },
+  },
 
-    // Signup validation
-    validateSignup: () => {
-        const { form } = get();
-        const newErrors = {};
+  // Signup validation
+  validateSignup: () => {
+    const { form } = get();
+    const newErrors = {};
 
-        if (!form.name.trim()) newErrors.name = i18n.t('nameRequired');
+    if (!form.name.trim()) newErrors.name = i18n.t('nameRequired');
 
-        if (!form.name.trim()) newErrors.name = "nameRequired";
+    if (!form.email.trim()) newErrors.email = i18n.t('emailRequired');
+    else if (!/\S+@\S+\.\S+/.test(form.email)) newErrors.email = i18n.t('emailInvalid');
 
-        if (!form.email.trim()) newErrors.email = 'emailRequired';
-        else if (!/\S+@\S+\.\S+/.test(form.email))
-        newErrors.email = i18n.t('emailInvalid');
+    if (!form.password) newErrors.password = i18n.t('passwordRequired');
 
-        if (!form.password) newErrors.password ='passwordRequired';
-        
-        if(!form.confirmPassword) 
-            newErrors.confirmPassword ='confirmPasswordRequired';
+    if (!form.confirmPassword) newErrors.confirmPassword = i18n.t('confirmPasswordRequired');
 
-        if(form.password && form.confirmPassword && 
-            form.password !== form.confirmPassword){
-                newErrors.confirmPassword='passwordsDoNotMatch';
-        }
+    if (form.password && form.confirmPassword && form.password !== form.confirmPassword) {
+      newErrors.confirmPassword = i18n.t('passwordsDoNotMatch');
+    }
 
-        if (!form.phone) newErrors.phone = 'phoneRequired';
-        else if (!/^7\d{8}$/.test(form.phone))
-            newErrors.phone = 'phoneInvalid';
-        return newErrors;
-    },
+    if (!form.phone) newErrors.phone = i18n.t('phoneRequired');
+    else if (!/^7\d{8}$/.test(form.phone)) newErrors.phone = i18n.t('phoneInvalid');
 
-    // submit signup
-    signupUser: async () => {
-        const { form, validateSignup, setErrors, setLoading, resetForm} = get();
+    return newErrors;
+  },
 
-        const validationErrors = validateSignup();
+  // submit signup
+  signupUser: async () => {
+    const { form, validateSignup, setErrors, setLoading, resetForm } = get();
 
-            if (Object.keys(validationErrors).length > 0) {
-            setErrors(validationErrors);
-            return {
-                success:false,
-                type:'validation'
-            };
-            }
-            console.log("setErrors: ",setErrors);
+    const validationErrors = validateSignup();
 
-        setErrors({});
-        setLoading(true);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return{
+        success:false,
+        type:'validation'
+      };
+    }
 
-        try {
-        const { name, email, password, phone } = form;
-        const data = await signup({ name, email, password, phone });
-            resetForm();   
-            return {
-                success:true,
-                message:getServerMessage(data),
-                data,
-            };
+    setErrors({});
+    setLoading(true);
 
-        } catch (err) {
-                let errorMessage;
-                if(err.name === "HTTPError"){
-                const errorData = await err.response.json().catch(()=>({message:err.message}));
-                errorMessage= getServerMessage(errorData);
-            }else{
-                errorMessage=err.message;
-            }
+    try {
+      const { name, email, password, phone } = form;
+      const data = await signup({ name, email, password, phone });
 
-            setErrors({
-                general:errorMessage || i18n.t('signupFailed')
-            });
+      return {
+        success:true,
+        message:getServerMessage(data),
+        data
+      }
+    } catch (err) {
+     
+      let errorMessage;
+      if (err.name === 'HTTPError') {
+        const errorData = await err.response.json().catch(() => ({ message: err.message }));
+        errorMessage = getServerMessage(errorData);
+      } else {
+        errorMessage = err.message;
+      }
+       setErrors({
+        general:errorMessage || i18n.t('signupFailed'),
+       });
 
-            return {
-                success:false,
-                message:errorMessage || i18n.t('signupFailed'),
-            }
-        } finally {
-            setLoading(false);
-        }
-    },
+       return {
+        success:false,
+        message:errorMessage || i18n.t('signupFailed')
+       }
+    } finally {
+      setLoading(false);
+    }
+  },
 
     // Login Validation
-    validateLogin: () => {
-        const { form } = get();
-        const newErrors = {};
-        if (!form.email.trim()) newErrors.email = i18n.t('emailRequired');
-        else if (!/\S+@\S+\.\S+/.test(form.email)) newErrors.email = i18n.t('emailInvalid');
-        if (!form.password) newErrors.password = i18n.t('passwordRequired');
-        else if (form.password.length < 8) newErrors.password = i18n.t('passwordTooShort');
+  validateLogin: () => {
+    const { form } = get();
+    const newErrors = {};
 
-        return newErrors;
-    },
+    if (!form.email.trim()) newErrors.email = i18n.t('emailRequired');
+    else if (!/\S+@\S+\.\S+/.test(form.email)) newErrors.email = i18n.t('emailInvalid');
+    if (!form.password) newErrors.password = i18n.t('passwordRequired');
+    else if (form.password.length < 8) newErrors.password = i18n.t('passwordTooShort');
 
+    return newErrors;
+  },
 
-    // Login Submit
-    loginUser: async()=>{
-        const {
-            form,
-            validateLogin,
-            setErrors,
-            setLoading,
-            setUser,
-            resetForm
-        } = get();
+  // Login Submit
+  loginUser: async () => {
+    const { 
+      form, 
+      validateLogin,
+      setErrors,
+      setLoading,
+      setUser, 
+      resetForm 
+    } = get();
 
+    const validationErrors = validateLogin();
 
-setErrors({});
-setLoading(true);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return{
+        success: false,
+        type: "validation"
+      };
+    }
 
-        if(Object.keys(validationErrors).length > 0){
-            setErrors(validationErrors);
-                return {
-                    success:false,
-                    type:'validation'
-                };
+    setErrors({});
+    setLoading(true);
+
+    try {
+      const { email, password } = form;
+
+      const response = await login({ email, password });
+
+     
+      if (response.success) {
+          const user = response.data || { email };
+          const token = response.data?.token || response.token;
+          setUser(user, token);
+          resetForm();
+
+          return {
+            success:true,
+            data:user,
+          };
+      } else {
+        return {
+          success:false,
+          message:getServerMessage(response)
         }
-        setErrors({});
-        setLoading(true);
+      }
+    } catch (err) {
+     
+      let errorMessage;
 
-        try{
-            const {email , password} = form;
-            const response = await login({email,password});
-            
-            if (response.success) {
-                const user= response.data || {email};
-                const token=response.data?.token || response.token;
-                setUser(user, token);
-                    resetForm();
-                    return {
-                    success:true,
-                    data:user
-                    };
-                }else{
-                    return {
-                        success:false,
-                        message:getServerMessage(response)
-                    };
-                }
-        }catch(err) {
+      if (err.name === 'HTTPError') {
+        const errorData = await err.response.json().catch(() => ({ message: err.message }));
+        errorMessage = getServerMessage(errorData);
+      } else {
+        errorMessage = getServerMessage({ message: err.message });
+      }
 
-            let errorMessage;
-            if (err.name === 'HTTPError') {
-                const errorData = await err.response.json().catch(() => ({ message: err.message }));
-                errorMessage = getServerMessage(errorData);
-            } else {
-                errorMessage = getServerMessage({ message: err.message });
-            }
-            setErrors({
-                general: errorMessage,
-            });
-            return {
-                success:false,
-                message:errorMessage
-            };
-        }finally{
-            setLoading(false);
-        }
-    },
+       setErrors({
+        general:errorMessage
+       });
 
-    // Logout
-    logout:(navigate)=>{
-        set({user:null,token:null});
-        localStorage.removeItem('user');
-        localStorage.removeItem('token');
-        navigate('/login');
-    },
+       return {
+        success:false,
+        message:errorMessage
+       }
 
-    hasError: (field) => {
-    const { errors } = get();
-    return !!errors[field] || !!errors.general;
-},
- 
+    } finally {
+      setLoading(false);
+    }
+  },
+
+  // Logout
+  logout: (navigate) => {
+    set({ user: null, token: null });
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    navigate('/login');
+  },
 }));
 
 export default useAuthStore;
