df73497e48e1bdfa3b17c342b126cf60e2a467b5
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-04-30 13:30:35 +0430

    import ROUTE_PATH and change path's value

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -5,32 +5,33 @@ import CheckEmail from '../pages/public/auth/CheckEmail';
 import ResetPassword from '../pages/public/auth/ResetPassword';
 import RequestResetPassword from '../pages/public/auth/RequestResetPassword';      
 import AccessDenied from '../pages/public/AccessDenied';
+import {ROUTE_PATHS} from './routePaths';
 
-const publicRoutes=[
-    {
-        path:"/signup",
-        element:<Signup/>
-    },
-    {
-        path:"/login",
-        element:<Login/>
-    },
-    {
-    path: "/check-email",
-    element: <CheckEmail />
-    },   
-    {
-        path:"/reset-password",
-        element:<ResetPassword/>
-    },
-    {
-        path: "/request-reset-password",
-        element: <RequestResetPassword />
-    },
-    {
-    path:"/access-denied",
-    element:<AccessDenied/>
-    }
+const publicRoutes = [
+  {
+    path: ROUTE_PATHS.SIGNUP,
+    element: <Signup />,
+  },
+  {
+    path: ROUTE_PATHS.LOGIN,
+    element: <Login />,
+  },
+  {
+    path: ROUTE_PATHS.CHECK_EMAIL,
+    element: <CheckEmail />,
+  },
+  {
+    path: ROUTE_PATHS.RESET_PASSWORD,
+    element: <ResetPassword />,
+  },
+  {
+    path: ROUTE_PATHS.REQUEST_RESET_PASSWORD,
+    element: <RequestResetPassword />,
+  },
+  {
+    path: ROUTE_PATHS.ACCESS_DENIED,
+    element: <AccessDenied />,
+  },
 ];
 
 export default publicRoutes;
