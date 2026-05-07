797882de8047fa7c1a2125084f33486620ac3f8a
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-05-07 12:02:27 +0430

    import and use token functions

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -1,4 +1,10 @@
 import ky from 'ky';
+import { 
+  getAccessToken,
+  getRefreshToken,
+  setTokens,
+  clearTokens
+ } from '../utils/tokenHelper';
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -11,17 +17,39 @@ const apiClient = ky.create({
   hooks: {
     beforeRequest: [
       (request) => {
-        const user = JSON.parse(localStorage.getItem("user"))
-        if(!user) return
-        if (user.token) {
-          request.headers.set('Authorization', `Bearer ${user.token}`);
+        const token = getAccessToken();
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`);
         }
       },
     ],
     afterResponse: [
-      (_request, response) => {
-        if (!response.ok && response.status === 401) {
-          console.log('Unauthorized! Redirect to login?');
+     async (request, options, response) => {
+        if ( response.status === 401) {
+          try{
+            const refreshToken = getRefreshToken ();
+            
+            if(!refreshToken) throw new Error('No refresh token');
+
+            const res = await ky.post('auth/refresh',{
+              preFixUrl:baseUrl,
+              json: {refreshToken},
+            }).json();
+
+            const newAccessToken = res.accessToken;
+
+            setTokens({
+              accessToken : newAccessToken,
+              refreshToken,
+            });
+
+            request.headers.set('Authorization',  `Bearer ${newAccessToken}`);
+
+            return ky(request);
+          }catch(error){
+            clearTokens();
+            window.location.href = '/login';
+          }
         }
 
         return response;
