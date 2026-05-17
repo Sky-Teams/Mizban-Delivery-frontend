26fbedf2127d9d8e3f64779b1d18c4bf2695a014
fataneh-mow <fataneh.mow@gmail.com>
2026-05-17 22:42:45 +0430

    fix: made the random id more unique

--- src/utils/getDeviceInfo.js
+++ src/utils/getDeviceInfo.js
@@ -2,7 +2,7 @@ export const getDeviceInfo = () => {
   let deviceId = localStorage.getItem('deviceId');
 
   if (!deviceId) {
-    deviceId = Date.now().toString();
+    deviceId = `${Date.now()}-${Math.floor(Math.random() * 1e6)}`;
 
     localStorage.setItem('deviceId', deviceId);
   }
