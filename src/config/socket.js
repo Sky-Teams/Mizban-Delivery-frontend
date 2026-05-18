032fa85c2707875be8539ef61e84394197fe6706
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-05-18 14:48:38 +0430

    update socket.js file

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,13 +1,33 @@
 import { io } from 'socket.io-client';
+import useAuthStore from '../store/useAuthStore';
+
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
+// create socket without token first
 export const socket = io(API_URL, {
-  auth: {
-    // the token
-    token: localStorage.getItem('token'),
-  },
+  autoConnect: false,  
 });
 
+// function to update token dynamically
+export const updateSocketToken = (token) => {
+  socket.auth = { token };
+
+  if (socket.connected) {
+    socket.disconnect();
+  }
+
+  socket.connect();
+};
+
+// optional: initialize once
+export const initSocket = () => {
+  const token = useAuthStore.getState().accessToken;
+
+  socket.auth = { token };
+  socket.connect();
+};
+
+// debug logs
 socket.on('connect', () => {
   console.log('socket connected', socket.id);
 });
