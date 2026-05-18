3dcd16b408aef1c44f4c28608196445a746b85ed
fataneh-mow <fataneh.mow@gmail.com>
2026-05-18 16:34:16 +0430

    fix: chnged ky to apiClient

--- src/services/deviceServices.js
+++ src/services/deviceServices.js
@@ -1,11 +1,8 @@
-import ky from 'ky';
-
-const localhost = 'http://localhost:3500/api'; // I tested the branch with an unmerged branch from back #101, to check the route /api/devices
+import apiClient from '../config/apiClient';
 
 export const registerDevice = async (deviceData) => {
-  return ky
-    .post(`${localhost}/devices`, {
-      // the route will be changes as soon as the api has the new route of devices
+  return apiClient
+    .post('devices', {
       json: deviceData,
     })
     .json();
