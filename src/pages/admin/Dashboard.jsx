a31795ef1bfb984dd880ca2fb4f0bf64ea22e0f2
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-15 16:46:50 +0430

    Use live tracking component in dashboards

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,15 @@
 import { useTranslation } from 'react-i18next';
+import { LiveLocation } from '../../components/liveDemo';
 
 export default function Dashboard() {
   const { t } = useTranslation();
 
-  return <div>{t('THIS_IS_DASHBOARD')}</div>;
+  return (
+    <div>
+      {t('THIS_IS_DASHBOARD')}
+      <div>
+        <LiveLocation />
+      </div>
+    </div>
+  );
 }
