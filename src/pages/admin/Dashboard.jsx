8e2246a2aca8967cd41645c7bfa66ec48d727a64
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-18 20:05:08 +0430

    remove live demo from dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,15 +1,7 @@
 import { useTranslation } from 'react-i18next';
-import { LiveLocation } from '../../components/liveDemo';
 
 export default function Dashboard() {
   const { t } = useTranslation();
 
-  return (
-    <div>
-      {t('THIS_IS_DASHBOARD')}
-      <div>
-        <LiveLocation />
-      </div>
-    </div>
-  );
+  return <div>{t('THIS_IS_DASHBOARD')}</div>;
 }
