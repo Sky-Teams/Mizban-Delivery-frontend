87b2c0cb63774a73d6d6d2240b47ef936c178c72
fataneh-mow <fataneh.mow@gmail.com>
2026-07-27 10:23:11 +0430

    fix: added ? as a fallback to prevent the page craash #93

--- src/components/common/Driver/DriverOrderStatusBar.jsx
+++ src/components/common/Driver/DriverOrderStatusBar.jsx
@@ -11,11 +11,11 @@ export default function DriverOrderStatusbar({ orders = [], currentStatus, setCu
   const activeButton = 'text-orange-500 border-b-2 border-orange-500 font-semibold';
 
   // filtering the data that is comign from the backend,
-  const completedOrders = orders.filter((order) => order.status === 'delivered');
-  const cancelledOrders = orders.filter((order) => order.status === 'cancelled');
-  const rejectedOrders = orders.filter((order) => order.status === 'rejected');
-  const expiredOrders = orders.filter((order) => order.offer.status === 'expired');
-  const returnedOrders = orders.filter((order) => order.status === 'returned');
+  const completedOrders = orders.filter((order) => order?.status === 'delivered');
+  const cancelledOrders = orders.filter((order) => order?.status === 'cancelled');
+  const rejectedOrders = orders.filter((order) => order?.status === 'rejected');
+  const expiredOrders = orders.filter((order) => order?.offer.status === 'expired');
+  const returnedOrders = orders.filter((order) => order?.status === 'returned');
 
   const handleStatusButtonsClick = (status) => {
     setCurrentStatus(status);
