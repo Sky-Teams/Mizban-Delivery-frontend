8fe80c5992c455b24dea615c70484f224a9da2d6
fataneh-mow <fataneh.mow@gmail.com>
2026-08-02 13:08:41 +0430

    feat: added a button for navigating to the drivers requests page #103

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDriverStore } from '../../store/driver/useDriverStore';
+import PendingDriverGrid from '../../components/admin/driverVerification/PendingDriverGrid';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/common/order/Button';
 
 export default function Dashboard() {
-  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const {t} = useTranslation()
 
-  return <div>{t('THIS_IS_DASHBOARD')}</div>;
+  const handleDriverVerificationHandle = () => {
+    navigate('driver-verification');
+  };
+  
+  return (
+    <div className="flex items-center justify-between">
+      <h1>{t('THIS_IS_DASHBOARD')}</h1>
+      <Button text={'Driver Verification'} onClick={handleDriverVerificationHandle} />
+    </div>
+  );
 }
