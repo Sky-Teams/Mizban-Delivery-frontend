231bd22021c76a63905d21ce40fe753c59b008f3
fataneh-mow <fataneh.mow@gmail.com>
2026-08-07 22:11:51 +0430

    fix: changed the translation method due to react hook strictness #105

--- src/services/listener/notificationListener.js
+++ src/services/listener/notificationListener.js
@@ -1,14 +1,13 @@
-import { useTranslation } from 'react-i18next';
 import { socket } from '../../config/socket';
 import { useNotificationStore } from '../../store/notificationInbox/useNotificationStore';
+import i18n from '../../i18n';
 
 export const notificationListener = () => {
-  const { t } = useTranslation();
   socket.off('notification'); // to prevent duplicates
   socket.on('notification', (data) => {
     useNotificationStore.getState().addNotification({
       id: data.orderId ?? Date.now(),
-      message: data?.message || t('NEW_EVENT_ARRIVED'),
+      message: data?.message || i18n.t('NEW_EVENT_ARRIVED'),
     });
   });
 };
