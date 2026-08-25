82ccb089e31a13375c46b0cf1abd1df0372d5d97
fataneh-mow <fataneh.mow@gmail.com>
2026-08-25 22:51:54 +0430

    feat: made the socket to listen for offer event #113

--- src/services/listener/notificationListener.js
+++ src/services/listener/notificationListener.js
@@ -5,10 +5,23 @@ import toast from 'react-hot-toast';
 
 export const notificationListener = () => {
   socket.off('notification'); // to prevent duplicates
+  socket.off('offer');
+
   socket.on('notification', (data) => {
     useNotificationStore.getState().addNotification({
       id: data.orderId ?? Date.now(),
       message: data?.message || t('NEW_EVENT_ARRIVED'),
+    });
+  });
+
+  socket.on('offer', (data) => {
+    const offer = Array.isArray(data) ? data[0] : data;
+
+    useNotificationStore.getState().addOffer({
+      id: offer?.offerId ?? Date.now(),
+      type: offer?.type,
+      title: offer?.title,
+      message: offer?.message,
     });
   });
 };
